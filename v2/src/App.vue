69f5bebe68e6bbd7f761eb9d2b185e718fd83ecc
Enubia <44695100+Enubia@users.noreply.github.com>
2023-02-22 21:41:10 +0100

    menu buttons

--- v2/src/App.vue
+++ v2/src/App.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue';
+import MenuButtons from './components/MenuButtons.vue';
+
+postMessage({ payload: 'removeLoading' }, '*');
+
+// const version = process.;
+
+// console.log('[App.vue]', `Hello world from Electron ${process.versions.electron}!`);
 </script>
 
 <template>
-	<HelloWorld msg="Electron + Vite + Vue" />
-	<div class="center-elements">
-		Place static files into the <code>/public</code> folder
-		<img style="width: 5em" src="/img/icons/icon-256x256.png" alt="Node logo" />
-	</div>
+	<header>
+		<div id="app-info">Here comes the version</div>
+		<MenuButtons />
+	</header>
+	<main class="container-fluid">
+		<HelloWorld msg="Electron + Vite + Vue" />
+		<div class="center-elements">
+			Place static files into the <code>/public</code> folder
+			<img style="width: 5em" src="/img/icons/icon-256x256.png" alt="Node logo" />
+		</div>
+	</main>
 </template>
---
+++ v2/src/components/MenuButtons.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isChatPage = ref(true);
+</script>
+
+<template>
+	<div v-if="isChatPage" id="menu-buttons">
+		<button class="secondary">
+			<img id="back" src="../assets/svg/back.svg" />
+		</button>
+		<button class="secondary">
+			<img id="back" src="../assets/svg/minimize.svg" />
+		</button>
+		<button class="secondary">
+			<img id="back" src="../assets/svg/close.svg" />
+		</button>
+	</div>
+</template>
