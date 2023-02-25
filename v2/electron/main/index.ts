779facc383e655239885b3b8153721b74c7f7ce4
Enubia <44695100+Enubia@users.noreply.github.com>
2023-02-25 11:12:21 +0100

    revert window stuff

--- v2/electron/main/index.ts
+++ v2/electron/main/index.ts
@@ -1,12 +1,11 @@
 import { release } from 'node:os';
 import { join } from 'node:path';
 
-import { app, BrowserWindow, Tray, ipcMain } from 'electron';
+import { app, BrowserWindow, Tray, ipcMain, Menu, shell } from 'electron';
 
 import { IpcConstants } from '../../shared/constants';
 
 import createStore from './appStore';
-import createWindow from './window';
 
 process.env.DIST_ELECTRON = join(__dirname, '..');
 process.env.DIST = join(process.env.DIST_ELECTRON, '../dist');
@@ -34,6 +33,122 @@ const store = createStore();
 
 // const preload = join(__dirname, '../preload/index.js');
 const indexHtml = join(process.env.DIST, 'index.html');
+
+export default function createWindow() {
+	const windowState = store.get('savedWindowState');
+
+	// eslint-disable-next-line no-param-reassign
+	window = new BrowserWindow({
+		title: 'Ghost Chat',
+		x: windowState.x,
+		y: windowState.y,
+		width: windowState.width || 400,
+		height: windowState.height || 800,
+		transparent: windowState.isTransparent,
+		frame: false,
+		resizable: true,
+		maximizable: false,
+		webPreferences: {
+			// preload,
+			webviewTag: true,
+			nodeIntegration: true,
+			contextIsolation: false,
+		},
+	});
+
+	if (process.platform !== 'darwin') {
+		window.setAlwaysOnTop(true, 'pop-up-menu');
+	} else {
+		app.dock.hide();
+		window.setAlwaysOnTop(true, 'floating');
+		window.setVisibleOnAllWorkspaces(true);
+		window.setFullScreenable(false);
+	}
+
+	if (windowState.x === 0 && windowState.y === 0) {
+		// center the window on initial launch
+		window.center();
+	}
+
+	if (process.env.VITE_DEV_SERVER_URL) {
+		window.loadURL(process.env.VITE_DEV_SERVER_URL);
+		window.webContents.openDevTools({
+			mode: 'bottom',
+		});
+	} else {
+		window.loadFile(indexHtml);
+	}
+
+	window.webContents.on('did-finish-load', () => {
+		window?.webContents.send('get-version', app.getVersion());
+	});
+
+	// Make all links open with the browser, not with the application
+	window.webContents.on('will-navigate', (event, url) => {
+		event.preventDefault();
+		shell.openExternal(url);
+	});
+
+	const trayIcnName = 'trayicon.png';
+	const trayIcnPath = `${process.env.PUBLIC}/${trayIcnName}`;
+
+	// eslint-disable-next-line no-param-reassign
+	tray = new Tray(trayIcnPath);
+
+	const trayIconMenu = Menu.buildFromTemplate([
+		{
+			label: 'Revert Vanish',
+			type: 'normal',
+			click: async () => {
+				store.set('savedWindowState.clickThrough', false);
+				store.set('savedWindowState.isTransparent', false);
+				window?.setIgnoreMouseEvents(false);
+				app.relaunch();
+				app.exit();
+			},
+		},
+		{
+			label: 'Revert ClickThrough',
+			type: 'normal',
+			click: async () => {
+				window?.setIgnoreMouseEvents(false);
+			},
+		},
+		{
+			label: 'Quit Ghost Chat',
+			click: async () => {
+				window?.close();
+			},
+		},
+	]);
+
+	tray?.setToolTip('Ghost Chat');
+	tray?.setContextMenu(trayIconMenu);
+
+	window.on('close', () => {
+		if (window) {
+			const windowBounds = window.getBounds();
+
+			const windowState = {
+				x: windowBounds.x,
+				y: windowBounds.y,
+				width: windowBounds.width,
+				height: windowBounds.height,
+				isClickThrough: false,
+				isTransparent: false,
+			};
+
+			store.set('savedWindowState', windowState);
+		} else {
+			// if the window should be null reset the window state entirely just in case
+			store.reset('savedWindowState');
+		}
+	});
+
+	window.on('closed', () => {
+		store.set('channelOptions.channel', '');
+	});
+}
 
 // ---------------------------------- ipc handling ----------------------------------
 
@@ -97,9 +212,9 @@ app.on('activate', async () => {
 		if (allWindows.length) {
 			allWindows[0].focus();
 		} else {
-			createWindow(app, window, store, tray, indexHtml);
+			createWindow();
 		}
 	}
 });
 
-app.whenReady().then(() => createWindow(app, window, store, tray, indexHtml));
+app.whenReady().then(createWindow);
