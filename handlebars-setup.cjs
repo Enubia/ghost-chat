8e0598e6403299f7c673d9c39e047435f33618c8
Enubia <44695100+Enubia@users.noreply.github.com>
2025-04-20 09:43:21 +0200

    refactor: simplify commit type handling in formatMessage function

--- handlebars-setup.cjs
+++ handlebars-setup.cjs
@@ -14,32 +14,25 @@ function formatMessage(string) {
 
     const [commitType, rest] = string.split(':').map(part => part.trim());
 
-    if (commitType.startsWith('new') || commitType.startsWith('feat')) {
-        return `<span class="text-green-400 bg-green-900 px-1">${commitType}</span>: ${rest}`;
-    }
-
-    if (commitType.startsWith('refactor') || commitType.startsWith('deps')) {
-        return `<span class="text-purple-400 bg-purple-900 px-1">${commitType}</span>: ${rest}`;
-    }
-
-    if (commitType.startsWith('docs')) {
-        return `<span class="text-blue-400 bg-blue-900 px-1">${commitType}</span>: ${rest}`;
-    }
-
-    if (commitType.startsWith('update')) {
-        return `<span class="text-pink-400 bg-pink-900 px-1">${commitType}</span>: ${rest}`;
-    }
-
-    if (commitType.startsWith('chore')) {
-        return `<span class="text-blue-400 bg-blue-900 px-1">${commitType}</span>: ${rest}`;
-    }
-
-    if (commitType.startsWith('fix')) {
-        return `<span class="text-yellow-400 bg-yellow-900 px-1">${commitType}</span>: ${rest}`;
-    }
-
-    if (commitType.startsWith('ci')) {
-        return `<span class="text-gray-400 bg-gray-900 px-1">${[commitType]}</span>: ${rest}`;
+    switch (commitType) {
+        case 'new':
+        case 'feat':
+            return `<span class="text-green-400 bg-green-900 px-1">${commitType}</span>: ${rest}`;
+        case 'refactor':
+        case 'deps':
+            return `<span class="text-purple-400 bg-purple-900 px-1">${commitType}</span>: ${rest}`;
+        case 'docs':
+            return `<span class="text-blue-400 bg-blue-900 px-1">${commitType}</span>: ${rest}`;
+        case 'update':
+            return `<span class="text-pink-400 bg-pink-900 px-1">${commitType}</span>: ${rest}`;
+        case 'chore':
+            return `<span class="text-blue-400 bg-blue-900 px-1">${commitType}</span>: ${rest}`;
+        case 'fix':
+            return `<span class="text-yellow-400 bg-yellow-900 px-1">${commitType}</span>: ${rest}`;
+        case 'ci':
+            return `<span class="text-gray-400 bg-gray-900 px-1">${commitType}</span>: ${rest}`;
+        default:
+            break;
     }
 
     return string;
