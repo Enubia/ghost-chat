a8d1a20abd61aeb6e3d6e2ee42953ab998890075
Enubia <44695100+Enubia@users.noreply.github.com>
2025-04-23 13:21:32 +0200

    chore: link update

--- shared/constants/links.ts
+++ shared/constants/links.ts
@@ -1,3 +1,3 @@
-export const downloadLink = 'https://github.com/enubia/ghost-chat/releases/latest';
+export const downloadLink = 'https://github.com/enubia/ghost-chat/releases/latest?referrer=ghost-chat-download-notification';
 export const paypalLink = 'https://www.paypal.com/donate/?hosted_button_id=JMYLMVGSKXXEW';
 export const kofiLink = 'https://ko-fi.com/enubia';
