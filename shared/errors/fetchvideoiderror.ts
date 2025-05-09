47ae7a8570ec8fc652568d3474691bc41dcd53d2
Enubia <44695100+Enubia@users.noreply.github.com>
2025-05-09 08:05:26 +0200

    refactor: remove default error messages

--- shared/errors/fetchvideoiderror.ts
+++ shared/errors/fetchvideoiderror.ts
@@ -1,6 +1,5 @@
 export default class FetchVideoIdError extends Error {
     constructor() {
         super();
-        this.message = 'failed-to-fetch-video-id';
     }
 }
--- shared/errors/streamnotstartederror.ts
+++ shared/errors/streamnotstartederror.ts
@@ -1,6 +1,5 @@
 export default class StreamNotStartedError extends Error {
     constructor() {
         super();
-        this.message = 'stream-not-started';
     }
 }
